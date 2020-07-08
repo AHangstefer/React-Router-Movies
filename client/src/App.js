5ecfcda7a5a85ca4dbff2dacd29834c354a6a127
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-08 16:19:58 -0400

    adding link and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Router, Route, Link } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +30,19 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>Replace this Div with your Routes. One route for / that loads 
+        the MovieList component. This component will need the movies injected 
+        into it via props.
+        one route that will take an id parameter after/movies/
+         (ex: /movies/2, /movies/3 where the id is dynamic). 
+         This route should load the Movie component.
+      </div>
+      <Router>
+        <Link to="/MovieList">MovieList</Link>
+        <Link to="/Movie">Movies</Link>
+        <Route path="/MovieList"> component = {MovieList}</Route>
+        <Route path="/Movie"> component = {Movie} </Route>
+      </Router>
     </div>
   );
 };
