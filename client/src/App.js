262e75b541470c57f1c85848eb4005ca41bf4b11
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-07-08 21:27:13 -0400

    edits in app, movielist

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -30,19 +30,19 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes. One route for / that loads 
+          {/*  <div>  Replace this Div with your Routes. One route for / that loads 
         the MovieList component. This component will need the movies injected 
         into it via props.
         one route that will take an id parameter after/movies/
          (ex: /movies/2, /movies/3 where the id is dynamic). 
-         This route should load the Movie component.
-      </div>
-      <Router>
-        <Link to="/MovieList">MovieList</Link>
-        <Link to="/Movie">Movies</Link>
-        <Route path="/MovieList"> component = {MovieList}</Route>
-        <Route path="/Movie"> component = {Movie} </Route>
-      </Router>
+         This route should load the Movie component. 
+           </div>   */}
+           <Link to ="/Movie/0">Movie0</Link>
+       <Switch>
+        <Route path="/Movie/:id"> component = {Movie} </Route> 
+        <Route path="/"> component = {MovieList}</Route>
+       </Switch> 
+      
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import MovieCard from "./MovieCard";
+import {Link} from "react-router-dom";
 
 const MovieList = props => {
   return (
@@ -11,25 +13,11 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+  return(
+    <Link to ={`/Movies/${movie.id}`}><MovieCard movie={movie}
+     
+    /></Link>
+  )
 }
 
 export default MovieList;
